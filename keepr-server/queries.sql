ca7c5d676ad0eae99c84cb07e08368f854b5cd54
briglowell <brig.lowell@gmail.com>
2021-01-05 14:08:26 -0700

    initialized database tables

--- keepr-server/queries.sql
+++ keepr-server/queries.sql
@@ -1,37 +1,43 @@
 -- CREATE TABLE keeps (
 --     id INT NOT NULL AUTO_INCREMENT,
---     creatorId VARCHAR(255),
---     name VARCHAR(80),
---     description VARCHAR(1000),
+--     creatorId VARCHAR(255) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     description VARCHAR(1000) NOT NULL,
 --     img VARCHAR(255),
 --     views INT NOT NULL,
 --     shares INT NOT NULL,
 --     PRIMARY KEY (id),
---     FOREIGN KEY (creatorId) references profile(id) on delete cascade
+--     FOREIGN KEY (creatorId) REFERENCES profiles (id) ON DELETE CASCADE
 -- )
 
 -- CREATE TABLE vaults(
 --     id INT NOT NULL AUTO_INCREMENT,
---     creatorId VARCHAR(255),
---     name VARCHAR(80),
+--     creatorId VARCHAR(255) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
 --     description VARCHAR(255),
---     isPrivate TINYINT,
---     PRIMARY KEY (id)
+--     isPrivate TINYINT NOT NULL,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (creatorId) REFERENCES profiles (id) ON DELETE CASCADE
 -- )
+
+-- drop table profile;
 
 -- CREATE TABLE vaultkeeps (
 --     id INT NOT NULL AUTO_INCREMENT,
---     creatorId VARCHAR(255),
---     vaultId VARCHAR(255),
---     keepId VARCHAR(255),
---     PRIMARY KEY (id)
+--     creatorId VARCHAR(255) NOT NULL,
+--     vaultId INT NOT NULL,
+--     keepId INT NOT NULL,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (creatorId) REFERENCES profiles (id) ON DELETE CASCADE,
+--     FOREIGN KEY (vaultId) REFERENCES vaults (id) ON DELETE CASCADE,
+--     FOREIGN KEY (keepId) REFERENCES keeps (id) ON DELETE CASCADE
 -- )
 
--- CREATE TABLE profile (
---     id VARCHAR(255),
---     name VARCHAR(80),
---     email VARCHAR(255),
---     picture VARCHAR(255),
+-- CREATE TABLE profiles (
+--     id VARCHAR(255) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL,
+--     picture VARCHAR(255) NOT NULL,
 --     PRIMARY KEY (id)
 -- )
 
