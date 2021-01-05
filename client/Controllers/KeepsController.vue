093a22e9bc0c98263e8c51f47140d8203432940e
briglowell <brig.lowell@gmail.com>
2021-01-05 11:51:25 -0700

    setup

---
+++ client/Controllers/KeepsController.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="keeps-controller">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KeepsController',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/Controllers/VaultsController.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="vaults-controller">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VaultsController',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
